3264810af3c1cf3322057258803a20f55d90786f
EricKwak123 <113388481+EricKwak123@users.noreply.github.com>
2026-04-19 17:07:01 -0400

    Add queries to analyze mental health consequences by gender

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,1 +1,14 @@
-
+SELECT Gender_Clean, mental_health_consequence, COUNT(*) AS response_count
+FROM mental_health
+GROUP BY Gender_Clean, mental_health_consequence
+ORDER BY Gender_Clean, mental_health_consequence;
+-- adding numerical comparison to quantify consequence
+SELECT Gender_Clean,
+       AVG(CASE 
+           WHEN mental_health_consequence = 'No' THEN 0
+           WHEN mental_health_consequence = 'Maybe' THEN 50
+           WHEN mental_health_consequence = 'Yes' THEN 100
+       END) AS avg_consequence_score
+FROM mental_health
+GROUP BY Gender_Clean
+ORDER BY avg_consequence_score DESC;
